d750c711923aede0ba2850c515b474ee2d780534
Leonardo Pego Martins <leonardopegomartins@gmail.com>
2020-07-03 20:10:09 -0300

    CardWar 1.0

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var LeftPoints: UILabel!
+    
+    @IBOutlet weak var RightPoints: UILabel!
+    
+    @IBOutlet weak var LeftCard: UIImageView!
+    
+    @IBOutlet weak var RightCard: UIImageView!
+    
+    var scoreLeft : Int = 0
+    var scoreRight : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func DealButton(_ sender: Any) {
+        
+        let cardNumberLeft = Int.random(in: 2...14)
+        let cardNumberRight = Int.random(in: 2...14)
+        
+        LeftCard.image = UIImage(named: "card\(cardNumberLeft)")
+        RightCard.image = UIImage(named: "card\(cardNumberRight)")
+        
+        if(cardNumberLeft > cardNumberRight){
+            scoreLeft += 1
+        } else if (cardNumberLeft < cardNumberRight){
+            scoreRight += 1
+        } else {
+            print("tie")
+        }
+        
+        LeftPoints.text = String(scoreLeft)
+        RightPoints.text = String(scoreRight)
+        
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        
+        scoreLeft = 0
+        scoreRight = 0
+        
+        LeftPoints.text = String(scoreLeft)
+        RightPoints.text = String(scoreRight)
+    }
+    
 }
 
